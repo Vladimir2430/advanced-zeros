1470fe2496e1877c6eaba6b1348f36ea457c30d2
Uladzimir <berkut2430@gmail.com>
2018-02-21 02:46:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+module.exports =function getZerosCount(number, base) {
+  var count = base;
+  var sum=0;
+  for(let i=2; i<=count/2; i++){
+    if(count%i==0){
+      count=Math.floor(count/i);
+      i=1;
+    }
+  }
+
+  var a=count;
+  while (number>count) {
+    sum+=Math.floor(number/count);
+    count*=a;
+  }
+  return sum;
 }
