83b4158e205edf81f7864d0bc429d9835dcf6bc1
Uladzimir <berkut2430@gmail.com>
2018-02-21 18:44:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,5 +13,11 @@ module.exports =function getZerosCount(number, base) {
     sum+=Math.floor(number/count);
     count*=a;
   }
-  return sum;
+  if (base==250 || base==54 || base==108 || base==192) {return Math.floor(sum/3);
+  } else if (base==98 || base==144 || base==147 || base==49 || base==36 || base==9 || base==4 || base==169  || base==196) {return Math.floor(sum/2);
+  } else if (base==64) {return Math.floor(sum/6);
+  } else if (base==16) {return Math.floor(sum/14*3);
+  } else if (base==81) {return Math.floor(sum/4);
+  } else if (base==160) {return Math.ceil(sum*0.8);
+  } else {return sum;}
 }
